6240fbe2dcaa9ee058902f7cc7407d96d311114e
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-19 14:03:44 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -4,17 +4,18 @@ import kotlin.text.toInt
 fun main(){
 
     print("Enter your age :   ")
-    var age = readLine()!!.toInt()
-    try {
-        age!!.toInt()
-
-    }catch (e:Exception){
-        println( "Error")
-    }
-    if (age > 18 ){
+   
+    try { 
+        var age = readLine()!!.toInt()
+        if (age > 18 ){
         println("Welcome")
     }
     else
         println("You are not old enough")
+
+    }catch (e:Exception){
+        println( "Error")
+    }
+    
 
 }
